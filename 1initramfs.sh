adfb5af520f396efe44cfbdec46c13ab58c86fa1
rysndavjd <rysndavjd@protonmail.com>
2024-10-13 01:26:23 +0200

    normal boot works

--- 1initramfs.sh
+++ 1initramfs.sh
@@ -1,78 +1,212 @@
 #!/bin/bash
 
 # Copyright 2024 rysndavjd
+# 1initramfs
 # Distributed under the terms of the GNU General Public License v2
 
-if [[ $EUID -ne 0 ]]
-then 
+if [[ $EUID -ne 0 ]] ; then 
     echo "Run as root."
     exit 1
 fi
 
 tmp=$(mktemp -d)
-echo $tmp
 shversion="git"
+. /etc/default/1initramfs 2>/dev/null
 
-help () {
+helpfn () {
     echo "1initramfs, version $shversion"
     echo "Usage: 1initramfs [option] ..."
     echo "Options:"
     echo "      -h  (calls help menu)"
+    echo "      -c  (Override using default config at /etc/default/1initramfs)"
+    echo "      -o  (Override default output directory at /boot)"
     exit 0
 }
 
-while getopts hc: flag ; do
+while getopts hc:o: flag ; do
     case "${flag}" in
-        ?) help;;
-        h) help;;
+        ?) helpfn;;
+        h) helpfn;;
         c) config="${OPTARG}";;
+        o) output="${OPTARG}";;
     esac
 done
+
+rootmnt=$(findmnt -n -o SOURCE /)
+rootuuid=$(blkid -s UUID -o value $rootmnt)
+rootluksuuid=$(cryptsetup luksUUID $rootmnt)
+cryptsetup isLuks $rootmnt
+rootisluks=$(echo $?)
+
+if [ -z "${output+x}" ] ; then
+    output="/boot"
+fi
+
+if [ -z "${RECOVERYSH+x}" ] ; then
+    RECOVERYSH="n"
+fi
+
+if [ -z "${YUBIOVERIDE+x}" ] ; then
+    YUBIOVERIDE="n"
+fi
 
 if ! [ -e $config ] ; then
     echo "Specified config does not exist at $config."
     exit 1
 fi
 
-if zcat /proc/config.gz | grep -q CONFIG_DEVTMPFS || cat /boot/config* | grep -q CONFIG_DEVTMPFS || cat /proc/mounts | grep -q devtmpfs ; then
+if cat /proc/mounts | grep -q devtmpfs || zcat /proc/config.gz | grep -q CONFIG_DEVTMPFS || cat /boot/config* | grep -q CONFIG_DEVTMPFS ; then
     dev="devtmpfs"
 else
-    dev="manual"
+    dev="mdev"
 fi
 
-rootmnt=$(findmnt -n -o SOURCE /)
-rootuuid=$(blkid -s UUID -o value $mnt)
-rootisluks=$(cryptsetup isLuks $rootmnt)
-
 # $1 = absolute path of binary
-copybinsfn() 
-{
+copybinsfn() {
     for num in $1 ; do
         dir=$(dirname "$num")
-        mkdir -p $tmp/$dir
-        cp $num $tmp/$dir
+        mkdir -p $tmp/build/$dir
+        cp $num $tmp/build/$dir
     done
 
     libs=$(ldd $1 | awk '/ => / { print $3 }' | paste -sd ' ' -)
     if ldd $1 | grep -q /lib64/ld-linux-x86-64.so ; then 
         libs="$libs /lib64/ld-linux-x86-64.so.2"
     fi
+
     if ldd $1 | grep -q /lib/ld-linux.so.2 ; then 
         libs="$libs /lib/ld-linux.so.2"
     fi
 
     for num in $libs ; do
         dir=$(dirname "$num")
-        mkdir -p $tmp/$dir
-        cp $num $tmp/$dir
+        mkdir -p $tmp/build/$dir
+        cp $num $tmp/build/$dir
     done
 }
 
-. /etc/default/1initramfs
+yubikeyfn () {
+if [ -z "${YUBIOVERIDE+x}" ] || [ $YUBIOVERIDE="n" ] ; then
+    YUBIOVERIDE=n
+    echo "Yubikey Challenge disabled."
+else
+    if [ $rootisluks=1 ] ; then
+        echo "Root is not LUKS, yubikey overide is useless."
+        exit 1
+    elif [ -z "${YUBICHAL+x}" ] ; then
+        echo "Yubikey challenge not set."
+        exit 1
+    elif [ $YUBICHAL="manual" ] ; then
+        echo "Yubikey Challenge set to manual, challenge will be entered on boot."
+    else
+        echo "Yubikey Challenge set to $YUBICHAL."
+    fi
+fi
 
-echo -n "$"
+}
+
+compressionfn () {
+if [ -z "${COMPRESSION+x}" ] ; then
+    COMPRESSION=none
+    echo "Compression not set, defaulting to none."
+else
+    echo "Compression set to $COMPRESSION."
+fi
+
+cd "$tmp/build"
+if [ $COMPRESSION = "none" ] ; then 
+    find . -print0 | cpio --null --create --verbose --format=newc > $output/initramfs.img 2>/dev/null
+fi
+}
+
+buildbasefn() {
+mkdir -p "$tmp"/build/{bin,dev,etc,lib,lib64,mnt/root,proc,root,sbin,sys,run}
+copybinsfn $(which busybox) 2>/dev/null
+
+cd $tmp/build/bin
+ln -sr ../usr/sbin/busybox busybox
+
+if [ $rootisluks = 0 ] ; then
+    copybinsfn $(which cryptsetup) 2>/dev/null
+fi
+
+if [ $YUBIOVERIDE = "y" ] ; then
+    copybinsfn $(which ykchalresp) 2>/dev/null
+    copybinsfn $(which lsusb) 2>/dev/null
+fi
+
+}
+
+buildinitfn() {
+touch $tmp/build/init
+chmod +x $tmp/build/init
+
+#Header
+echo "#!/bin/busybox sh" >> $tmp/build/init
+
+#Kernel FS
+echo "mount -n -t proc proc /proc
+mount -n -t sysfs sys /sys" >> $tmp/build/init
+
+#Devfs
+if [ $dev = "devtmpfs" ] ; then
+    echo "mount -n -t devtmpfs dev /dev" >> $tmp/build/init
+elif [ $dev = "mdev" ] ; then
+    echo "mount -t tmpfs dev /dev
+echo /sbin/mdev > /proc/sys/kernel/hotplug" >> $tmp/build/init
+fi
+
+#Rescue shell function
+echo "$RECOVERYSH"
+if [ $RECOVERYSH = "y" ] ; then
+    echo "rescue_shell() {
+    echo 0 > /proc/sys/kernel/printk
+    clear
+    echo \"Dropping to a shell: \$1\"
+    setsid cttyhack sh
+}" >> $tmp/build/init
+fi
+
+#Mounting real root
+if [ $rootisluks = 0 ] && [ $YUBIOVERIDE = "y" ] && [ $YUBICHAL = "manual" ] ; then
+#    echo "echo 0 > /proc/sys/kernel/printk
+#echo \"Overide, tap Yubikey to start decryption.\"
+#echo -n \"\$(ykchalresp -2H )\"" >> $tmp/build/init
+    echo "YUBICHAL=manual, still needs implementation"
+    exit 1
+elif [ $rootisluks = 0 ] && [ $YUBIOVERIDE = "y" ] ; then
+    echo "echo 0 > /proc/sys/kernel/printk
+lsusb | grep -q \"$YUBIUSB\"
+if [ \$? = 0 ] ; then
+    echo \"Overide, tap Yubikey to start decryption.\"
+    echo -n \"\$(ykchalresp -2H $YUBICHAL)\" | cryptsetup luksOpen \$(findfs UUID=$rootluksuuid) root
+else
+    cryptsetup luksOpen --tries 3 \$(findfs UUID=$rootluksuuid) root
+fi
+mount -o ro /dev/mapper/root /mnt/root" >> $tmp/build/init
+elif [ $rootisluks = 0 ] ; then
+    echo "echo 0 > /proc/sys/kernel/printk
+cryptsetup luksOpen --tries 3 \$(findfs UUID=$rootluksuuid) root
+mount -o ro /dev/mapper/root /mnt/root" >> $tmp/build/init
+else
+    echo "mount -o ro \$(findfs UUID=$rootuuid) /mnt/root" >> $tmp/build/init
+fi
 
 
-mkdir -p "$tmp"/{bin,dev,etc,lib,lib64,mnt/root,proc,root,sbin,sys,run}
+#Cleanup
+echo "umount /proc
+umount /sys
+umount /dev" >> $tmp/build/init
+
+#Switch to real root
+echo "exec switch_root /mnt/root /sbin/init" >> $tmp/build/init
+}
+
+echo "$tmp"
+buildbasefn
+buildinitfn
+compressionfn
+
+#echo -n "$YUBICHAL" | xxd -p -c 1 | awk '{$1=$1; gsub(/ /, ""); printf "%s", $0}' | ykman otp calculate 2
 
 
