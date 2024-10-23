c6a662b81814193324eaca3a2eba98dcfcfbf9ae
rysndavjd <rysndavjd@proton.me>
2024-10-23 07:49:01 +0200

    fixed spacing

--- 1initramfs.sh
+++ 1initramfs.sh
@@ -162,17 +162,17 @@ copybinsfn() {
 
 #function to minimise repeation of code for copying kernel modules
 kmodfn() {
-if modinfo -k $kernelver "$1" | grep -q "(builtin)" ; then
-    echo "$1 builtin."
-else
-    mkdir -p $tmp/build/$(dirname $(modinfo -k $kernelver -n "$1")) 
-    cp "$(modinfo -k $kernelver -n "$1")" "$tmp/build/$(modinfo -k $kernelver -n "$1")"
-    deps=$(modinfo "$1" | grep "depends:" | sed 's/,/ /g' | sed 's/depends://')
-    for item in $deps ; do
-        mkdir -p $tmp/build/$(dirname $(modinfo -k $kernelver -n $item)) 
-        cp "$(modinfo -k $kernelver -n $item)" "$tmp/build/$(modinfo -k $kernelver -n $item)"
-    done
-fi
+    if modinfo -k $kernelver "$1" | grep -q "(builtin)" ; then
+        echo "$1 builtin."
+    else
+        mkdir -p $tmp/build/$(dirname $(modinfo -k $kernelver -n "$1")) 
+        cp "$(modinfo -k $kernelver -n "$1")" "$tmp/build/$(modinfo -k $kernelver -n "$1")"
+        deps=$(modinfo "$1" | grep "depends:" | sed 's/,/ /g' | sed 's/depends://')
+        for item in $deps ; do
+            mkdir -p $tmp/build/$(dirname $(modinfo -k $kernelver -n $item)) 
+            cp "$(modinfo -k $kernelver -n $item)" "$tmp/build/$(modinfo -k $kernelver -n $item)"
+        done
+    fi
 }
 
 #resolves optimisation modules for crypto modules for specific x86 extensions (AVX2, AVX512, ssse3, etc)
@@ -192,168 +192,168 @@ fi
 
 #resolves needed crypto algoriums to decrypt rootfs
 copyalgokmodfn() {
-for item in $rootluksalgo ; do
-    case $item in 
-        *-xts-*)
-            kmodfn xts
-            case $item in
-                aes-*)
+    for item in $rootluksalgo ; do
+        case $item in 
+            *-xts-*)
+                kmodfn xts
+                case $item in
+                    aes-*)
 
-                ;;
-                serpent-*)
-                    
-                ;;
-                twofish-*)
+                    ;;
+                    serpent-*)
 
-                ;;
-                camellia-*)
+                    ;;
+                    twofish-*)
 
-                ;;
-                *)
-                    echo "Unknown Algorium used in xts mode of operation: $item" 
-                ;;
-            esac
-        ;;
-        *-cbc-*)
-            kmodfn cbc
-            case $item in
-                serpent-*)
-                    
-                ;;
-                *)
-                    echo "Unknown Algorium used in cbc mode of operation: $item" 
-                ;;
-            esac
-        ;;
-        *)
-            echo "Unknown mode of operation for encryption"
-            exit 1
-        ;;
-    esac
-done
+                    ;;
+                    camellia-*)
+
+                    ;;
+                    *)
+                        echo "Unknown Algorium used in xts mode of operation: $item" 
+                    ;;
+                esac
+            ;;
+            *-cbc-*)
+                kmodfn cbc
+                case $item in
+                    serpent-*)
+
+                    ;;
+                    *)
+                        echo "Unknown Algorium used in cbc mode of operation: $item" 
+                    ;;
+                esac
+            ;;
+            *)
+                echo "Unknown mode of operation for encryption"
+                exit 1
+            ;;
+        esac
+    done
 }
 
 #resolves fs module to read root fs
 copyfskmodfn() {
-mkdir -p "$tmp/build/lib/modules/$kernelver/kernel"
-cp /lib/modules/$kernelver/modules.* "$tmp/build/lib/modules/$kernelver/"
-case $rootfs in
-    ext4) 
-        kmodfn ext4
-    ;;
-    jfs) 
-        kmodfn jfs
-    ;;
-    xfs) 
-        kmodfn xfs
-    ;;
-    gfs2) 
-        kmodfn gfs2    
-    ;;
-    ocfs2) 
-        kmodfn ocfs2
-    ;;
-    btrfs) 
-        kmodfn btrfs
-    ;;
-    nilfs2) 
-        kmodfn nilfs2
-    ;;
-    f2fs) 
-        kmodfn f2fs
-    ;;
-    bcachefs) 
-        kmodfn bcachefs
-    ;;
-    ntfs) 
-        kmodfn ntfs3 
-    ;;
-    *) echo "Unknown fs: $rootfs" 
-    exit 1 
-    ;;
-esac
+    mkdir -p "$tmp/build/lib/modules/$kernelver/kernel"
+    cp /lib/modules/$kernelver/modules.* "$tmp/build/lib/modules/$kernelver/"
+    case $rootfs in
+        ext4) 
+            kmodfn ext4
+        ;;
+        jfs) 
+            kmodfn jfs
+        ;;
+        xfs) 
+            kmodfn xfs
+        ;;
+        gfs2) 
+            kmodfn gfs2    
+        ;;
+        ocfs2) 
+            kmodfn ocfs2
+        ;;
+        btrfs) 
+            kmodfn btrfs
+        ;;
+        nilfs2) 
+            kmodfn nilfs2
+        ;;
+        f2fs) 
+            kmodfn f2fs
+        ;;
+        bcachefs) 
+            kmodfn bcachefs
+        ;;
+        ntfs) 
+            kmodfn ntfs3 
+        ;;
+        *) echo "Unknown fs: $rootfs" 
+        exit 1 
+        ;;
+    esac
 }
 
 compressionfn () {
-if [ $KMODULES = "y" ] ; then
-    filename="initramfs-$kernelver.img"
-else 
-    filename="initramfs.img"
-fi
+    if [ $KMODULES = "y" ] ; then
+        filename="initramfs-$kernelver.img"
+    else 
+        filename="initramfs.img"
+    fi
 
-cd "$tmp/build"
-case $COMPRESSION in
-    none)
-        find . -print0 | cpio --quiet --null --create --format=newc > "$output/$filename" ;;
-    gzip)
-        find . -print0 | cpio --quiet --null --create --format=newc | gzip --quiet --stdout > "$output/$filename" ;;
-    bzip2)
-        find . -print0 | cpio --quiet --null --create --format=newc | bzip2 --quiet --compress --stdout > "$output/$filename" ;;
-    lzma)
-        find . -print0 | cpio --quiet --null --create --format=newc | xz --quiet --compress --format=lzma --stdout > "$output/$filename" ;;
-    lz4)
-        find . -print0 | cpio --quiet --null --create --format=newc | lz4 -z -q -l -c > "$output/$filename" ;;
-    zstd)
-        find . -print0 | cpio --quiet --null --create --format=newc | zstd --quiet --format=zstd --stdout > "$output/$filename" ;;
-    embedded)
-        find . -print0 | cpio --quiet --null --create --format=newc > "/usr/src/initramfs.cpio" ;;
-    *)
-        echo "Unknown: $COMPRESSION"
-        exit 1 
-    ;;
-esac
+    cd "$tmp/build"
+    case $COMPRESSION in
+        none)
+            find . -print0 | cpio --quiet --null --create --format=newc > "$output/$filename" ;;
+        gzip)
+            find . -print0 | cpio --quiet --null --create --format=newc | gzip --quiet --stdout > "$output/$filename" ;;
+        bzip2)
+            find . -print0 | cpio --quiet --null --create --format=newc | bzip2 --quiet --compress --stdout > "$output/$filename" ;;
+        lzma)
+            find . -print0 | cpio --quiet --null --create --format=newc | xz --quiet --compress --format=lzma --stdout > "$output/$filename" ;;
+        lz4)
+            find . -print0 | cpio --quiet --null --create --format=newc | lz4 -z -q -l -c > "$output/$filename" ;;
+        zstd)
+            find . -print0 | cpio --quiet --null --create --format=newc | zstd --quiet --format=zstd --stdout > "$output/$filename" ;;
+        embedded)
+            find . -print0 | cpio --quiet --null --create --format=newc > "/usr/src/initramfs.cpio" ;;
+        *)
+            echo "Unknown: $COMPRESSION"
+            exit 1 
+        ;;
+    esac
 }
 
 buildbasefn() {
-mkdir -p "$tmp"/build/{usr/bin,dev,etc,usr/lib,usr/lib64,mnt/root,proc,root,sys,run}
-cd $tmp/build/
-ln -sr ./usr/bin/ ./sbin
-ln -sr ./usr/bin/ ./bin
-ln -sr ./usr/bin/ ./usr/sbin
-ln -sr ./usr/lib/ ./lib
-ln -sr ./usr/lib64/ ./lib64
+    mkdir -p "$tmp"/build/{usr/bin,dev,etc,usr/lib,usr/lib64,mnt/root,proc,root,sys,run}
+    cd $tmp/build/
+    ln -sr ./usr/bin/ ./sbin
+    ln -sr ./usr/bin/ ./bin
+    ln -sr ./usr/bin/ ./usr/sbin
+    ln -sr ./usr/lib/ ./lib
+    ln -sr ./usr/lib64/ ./lib64
+    
+    mknod ./dev/kmsg c 1 11
+    mknod ./dev/console c 5 1
+    mknod ./dev/null c 1 3
+    mknod ./dev/zero c 1 5
+    mknod ./dev/random c 1 8
+    chmod 600 ./dev/*
+    
+    copybinsfn $(which busybox) 2>/dev/null
 
-mknod ./dev/kmsg c 1 11
-mknod ./dev/console c 5 1
-mknod ./dev/null c 1 3
-mknod ./dev/zero c 1 5
-mknod ./dev/random c 1 8
-chmod 600 ./dev/*
+    if [ $rootisluks = 0 ] ; then
+        copybinsfn $(which cryptsetup) 2>/dev/null
+    fi
 
-copybinsfn $(which busybox) 2>/dev/null
-
-if [ $rootisluks = 0 ] ; then
-    copybinsfn $(which cryptsetup) 2>/dev/null
-fi
-
-if [ $YUBIOVERIDE = "y" ] ; then
-    copybinsfn $(which ykchalresp) 2>/dev/null
-    copybinsfn $(which lsusb) 2>/dev/null
-fi
+    if [ $YUBIOVERIDE = "y" ] ; then
+        copybinsfn $(which ykchalresp) 2>/dev/null
+        copybinsfn $(which lsusb) 2>/dev/null
+    fi
 }
 
 buildinitfn() {
-touch $tmp/build/init
-chmod +x $tmp/build/init
+    touch $tmp/build/init
+    chmod +x $tmp/build/init
 
-#Header
-echo "#!/bin/busybox sh" >> $tmp/build/init
+    #Header
+    echo "#!/bin/busybox sh" >> $tmp/build/init
 
-#Kernel FS
-echo "mount -n -t proc proc /proc
+    #Kernel FS
+    echo "mount -n -t proc proc /proc
 mount -n -t sysfs sys /sys" >> $tmp/build/init
 
-#Devfs
-if [ $dev = "devtmpfs" ] ; then
-    echo "mount -n -t devtmpfs dev /dev" >> $tmp/build/init
-elif [ $dev = "mdev" ] ; then
-    echo "mount -t tmpfs dev /dev
+    #Devfs
+    if [ $dev = "devtmpfs" ] ; then
+        echo "mount -n -t devtmpfs dev /dev" >> $tmp/build/init
+    elif [ $dev = "mdev" ] ; then
+        echo "mount -t tmpfs dev /dev
 echo /sbin/mdev > /proc/sys/kernel/hotplug" >> $tmp/build/init
-fi
+    fi
 
-#Rescue shell function
-if [ $RECOVERYSH = "y" ] ; then
-    echo "rescue_shell() {
+    #Rescue shell function
+    if [ $RECOVERYSH = "y" ] ; then
+        echo "rescue_shell() {
     echo 0 > /proc/sys/kernel/printk
     busybox --install -s
     clear
@@ -361,33 +361,33 @@ if [ $RECOVERYSH = "y" ] ; then
     setsid cttyhack sh
 }" >> $tmp/build/init
 
-    echo "for item in \$(cat /proc/cmdline) ; do
+        echo "for item in \$(cat /proc/cmdline) ; do
     case "\$item" in
         rd.break|rdbreak)   rescue_shell \"Rescue rdbreak\" ;;
         init=/bin/sh|init=/bin/bb|init=/bin/bash|init=/bin/dash)    rescue_shell \"Rescue init=/bin/sh\" ;;
     esac
 done" >> $tmp/build/init
-fi
+    fi
 
-availfsmod="ext4 jfs xfs gfs2 ocfs2 btrfs nilfs2 f2fs bcachefs ntfs"
-# Inserting kernel modules
-if [ "$KMODULES" = "y" ] ; then
-    for item in $availfsmod ; do 
-        if echo "$item" | grep -q "$rootfs" ; then
-            echo "modprobe $item" >> $tmp/build/init
-        fi
-    done
-fi
+    availfsmod="ext4 jfs xfs gfs2 ocfs2 btrfs nilfs2 f2fs bcachefs ntfs"
+    # Inserting kernel modules
+    if [ "$KMODULES" = "y" ] ; then
+        for item in $availfsmod ; do 
+            if echo "$item" | grep -q "$rootfs" ; then
+                echo "modprobe $item" >> $tmp/build/init
+            fi
+        done
+    fi
 
-#Mounting real root
-if [ $rootisluks = "0" ] && [ $YUBIOVERIDE = "y" ] && [ $YUBICHAL = "manual" ] ; then
-#    echo "echo 0 > /proc/sys/kernel/printk
-#echo \"Overide, tap Yubikey to start decryption.\"
-#echo -n \"\$(ykchalresp -2H )\"" >> $tmp/build/init
-    echo "YUBICHAL=manual, still needs implementation"
-    exit 1
-elif [ $rootisluks = "0" ] && [ $YUBIOVERIDE = "y" ] ; then
-    echo "echo 0 > /proc/sys/kernel/printk
+    #Mounting real root
+    if [ $rootisluks = "0" ] && [ $YUBIOVERIDE = "y" ] && [ $YUBICHAL = "manual" ] ; then
+    #    echo "echo 0 > /proc/sys/kernel/printk
+    #echo \"Overide, tap Yubikey to start decryption.\"
+    #echo -n \"\$(ykchalresp -2H )\"" >> $tmp/build/init
+        echo "YUBICHAL=manual, still needs implementation"
+        exit 1
+    elif [ $rootisluks = "0" ] && [ $YUBIOVERIDE = "y" ] ; then
+        echo "echo 0 > /proc/sys/kernel/printk
 lsusb | grep -q \"$YUBIUSB\"
 if [ \$? = 0 ] ; then
     echo \"Overide, tap Yubikey to start decryption.\"
@@ -396,27 +396,27 @@ else
     cryptsetup luksOpen --tries 3 \$(findfs UUID=$rootluksuuid) root
 fi
 mount -o ro /dev/mapper/root /mnt/root" >> $tmp/build/init
-elif [ $rootisluks = "0" ] ; then
-    echo "echo 0 > /proc/sys/kernel/printk
+    elif [ $rootisluks = "0" ] ; then
+        echo "echo 0 > /proc/sys/kernel/printk
 cryptsetup luksOpen --tries 3 \$(findfs UUID=$rootluksuuid) root
 mount -o ro /dev/mapper/root /mnt/root" >> $tmp/build/init
-else
-    echo "mount -o ro \$(findfs UUID=$rootuuid) /mnt/root" >> $tmp/build/init
-fi
+    else
+        echo "mount -o ro \$(findfs UUID=$rootuuid) /mnt/root" >> $tmp/build/init
+    fi
 
 
-#Cleanup
-echo "umount /proc
+    #Cleanup
+    echo "umount /proc
 umount /sys
 umount /dev" >> $tmp/build/init
 
-#Switch to real root
-echo "exec switch_root /mnt/root /sbin/init" >> $tmp/build/init
+    #Switch to real root
+    echo "exec switch_root /mnt/root /sbin/init" >> $tmp/build/init
 
-#Rescue shell
-if [ $RECOVERYSH = "y" ] ; then
-    echo "rescue_shell \"Switch_Root_Failed\"" >> $tmp/build/init
-fi
+    #Rescue shell
+    if [ $RECOVERYSH = "y" ] ; then
+        echo "rescue_shell \"Switch_Root_Failed\"" >> $tmp/build/init
+    fi
 }
 
 buildbasefn
